0f429cf136f1e20600bf7c7475bea52ff55b2087
ShiranAvidov <shiran.avidov@logz.io>
2022-03-29 17:35:44 +0300

    script

--- mac.bash
+++ mac.bash
@@ -17,36 +17,36 @@ app_json=$1                             # Application JSON file
 
 #################################################### Build Paths to Manifest ####################################################
 
-dir1=$(jq -r .name $app_json)
-dir2=$(jq -r .subtype.name $app_json)
-dir3=$(jq -r .subtype.datasources[0].name $app_json)
+dir1=$(jq -r ".name $app_json")
+dir2=$(jq -r ".subtype.name $app_json")
+dir3=$(jq -r ".subtype.datasources[0].name $app_json")
 
 manifest="https://raw.githubusercontent.com/logzio/logzio-agent-manifest/init/$dir1/$dir2/$dir3"
-prerequisites=$(curl $manifest/prerequisites/mac.json?token=GHSAT0AAAAAABJEBVXLYPXRR7DJ3YIKATMOYSFR3AA | jq -r .commands)
-installer=$(curl $manifest/telemetry/installer/mac.json?token=GHSAT0AAAAAABJEBVXKOGVJYY46VA3AO3EGYSJRFLA | jq -r .commands)
+prerequisites=$(curl $manifest/prerequisites/mac.json?token=GHSAT0AAAAAABJEBVXLYPXRR7DJ3YIKATMOYSFR3AA | jq -r ".commands")
+installer=$(curl $manifest/telemetry/installer/mac.json?token=GHSAT0AAAAAABJEBVXKGQSM663BXP7ICA6UYSMJBJQ | jq -r ".commands")
 
 #################################################################################################################################
 
 ################################################## Run Prerequisites Commands ###################################################
 
-while read command; do
+while read -r command; do
     fullCommand=""
-    while read line; do
+    while read -r line; do
         fullCommand+=" $line"
-    done <<< "$(echo $command | jq -r .[])"
+    done < <(echo $command | jq -r ".[]")
     eval $fullCommand
-done <<< "$(echo $prerequisites | jq -c .[].run)"
+done < <(echo $prerequisites | jq -c ".[].run")
 
 #################################################################################################################################
 
 ##################################################### Run Installer Commands ####################################################
 
-while read command; do
+while read -r command; do
     fullCommand=""
-    while read line; do
+    while read -r line; do
         fullCommand+=" $line"
-    done <<< $(echo $command | jq -r .[])
+    done < <(echo $command | jq -r ".[]")
     eval $fullCommand
-done <<< $(echo $installer | jq -c .[].run)
+done < <(echo $installer | jq -c ".[].run")
 
 #################################################################################################################################
