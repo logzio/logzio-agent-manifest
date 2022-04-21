606679a47f61587495b69f6a7a6ed73adbd02a63
ShiranAvidov <48657224+ShiranAvidov@users.noreply.github.com>
2022-04-21 11:06:45 +0300

    Init (#1)
    * test
    
    * add
    
    * fix runs
    
    * mac
    
    * remove test
    
    * updated files
    
    * add installer json
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * metrics
    
    * updated
    
    * remove empty commands
    
    * update
    
    * add semicolon
    
    * test
    
    * test
    
    * fix
    
    * taints
    
    * taints
    
    * taints
    
    * metrics
    
    * installer
    
    * metrics
    
    * installer
    
    * installer
    
    * metrics
    
    * installer
    
    * installer
    
    * metrics
    
    * installer
    
    * metrics
    
    * installer
    
    * metrics
    
    * installer
    
    * script
    
    * logs
    
    * metrics
    
    * installer
    
    * remove raw tokens
    
    * changed secret names + helm install command
    
    * fix
    
    * add echo
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix if
    
    * fix vars
    
    * fix
    
    * fix
    
    * add helm set metrics env
    
    * fix
    
    * improve script + commands
    
    * test
    
    * test
    
    * fix
    
    * add brew install
    
    * multiline files
    
    * changed extension
    
    * tolerations + multiline
    
    * improvements + fix tolerations
    
    * traces
    
    * add traces
    
    * traces
    
    * linux
    
    * fix paths
    
    * fix
    
    * fix
    
    * fix
    
    * fix set
    
    * fix set
    
    * fix set
    
    * fix
    
    * fix
    
    * fix
    
    * fix helm install
    
    * fix
    
    * prerequisites
    
    * installer
    
    * add echo
    
    * fix
    
    * fix
    
    * fix
    
    * prerequisites
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * prerequisites
    
    * installer
    
    * logs
    
    * logs
    
    * logs
    
    * metrics
    
    * tracing
    
    * scripts
    
    * remove
    
    * installer
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * metrics
    
    * installer
    
    * installer
    
    * remove '
    
    * logs
    
    * installer
    
    * linux
    
    * scripts
    
    * metrics
    
    * scripts
    
    * fix tolerations
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * installer
    
    * script
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * installer
    
    * add region arg
    
    * add url arg
    
    * fix
    
    * add url arg
    
    * remove test
    
    * fix remove temp files
    
    * tolerations
    
    * tolerations
    
    * tolerations
    
    * tolerations
    
    * update args
    
    * readme
    
    * GCP
    
    * EKS
    
    * GKE
    
    * delete
    
    * add namespace
    
    * add namespace
    
    * add create-namespace to helm install
    
    * fix
    
    * readme
    
    * change branch to master

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# logzio-agent-manifest
+# Logz.io Agent Manifest
+
+-- This version is Beta and not yet ready for production --
+
+This repo contains the scripts and manifest files of the Logz.io agent.
---
+++ linux.bash
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+########################################################## Arguments ############################################################
+
+AGENT_ID=$1                             # Agent ID
+APP_JSON_FILE=$2                        # App JSON file (for tests)
+
+#################################################################################################################################
+
+######################################################## Helper Functions #######################################################
+
+# Runs commands
+# Inputs:
+# commands - The commands to run
+function run_commands () {
+    commands="$1"
+    while read -r command; do
+        full_command=""
+        while read -r line; do
+            full_command+=" $line"
+        done < <(echo "$command" | jq -r ".[]")
+        eval "$full_command"
+    done < <(echo "$commands" | jq -c ".[].run")
+}
+
+# Finds the requested parameter of a product in the application json 
+# Inputs: 
+# product_name - The product name (logs/metrics/traces/datasources)
+# product_params - The product parameters in the application json
+# requested_name - The parameter name to find
+# Output:
+# If the requested parameter was found, returns the parameter object with the requested_name,
+# otherwise exits with error
+function find_param () {
+    product_name="$1"
+    product_params="$2"
+    requested_name="$3"
+    requested_param=""
+    while read -r param; do
+        name=$(echo "$param" | jq -r ".name")
+        if [ "$name" = "$requested_name" ]; then
+            requested_param="$param"
+        fi
+    done < <(echo "$product_params" | jq -c ".[]")
+    echo "$requested_param"
+}
+
+#################################################################################################################################
+
+################################################## Prerequisites Installations ##################################################
+
+echo "Running prerequisites installations..."
+
+which jq >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "Installing JQ..."
+    which apt-get >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        sudo apt-get -y update
+        sudo apt-get install -y jq
+    else
+        which yum >/dev/null 2>&1
+        if [ $? -eq 0 ]; then
+            sudo yum -y update
+            sudo yum install -y jq
+        fi
+    fi
+else
+    echo "JQ is already installed"
+fi
+
+#################################################################################################################################
+
+########################################################## Get App JSON #########################################################
+
+if [ $# -eq 1 ]; then
+    echo "Getting app JSON from agent..."
+    APP_JSON=$(curl -LSs https://app.logz.io/telemetry-agent/public/agents/configuration/$AGENT_ID)
+else
+    echo "Using given app JSON..."
+    APP_JSON=$(cat $APP_JSON_FILE)
+fi
+
+#################################################################################################################################
+
+##################################################### Build Path to Manifest ####################################################
+
+echo "Building manifest path..."
+dir1=$(echo "$APP_JSON" | jq -r ".configuration.name")
+dir2=$(echo "$APP_JSON" | jq -r ".configuration.subtypes[0].name")
+dir3=$(echo "$APP_JSON" | jq -r ".configuration.subtypes[0].datasources[0].name")
+manifest="https://raw.githubusercontent.com/logzio/logzio-agent-manifest/init/$dir1/$dir2/$dir3"
+
+#################################################################################################################################
+
+########################################################## Get Commands #########################################################
+
+echo "Getting prerequisites commands..."
+prerequisites=$(curl -LSs $manifest/prerequisites/linux.json | jq -r ".commands")
+
+echo "Getting installer commands..."
+installer=$(curl -LSs $manifest/telemetry/installer/linux.json | jq -r ".commands")
+
+#################################################################################################################################
+
+################################################## Run Prerequisites Commands ###################################################
+
+echo "Running prerequisites commands..."
+run_commands "$prerequisites"
+
+#################################################################################################################################
+
+##################################################### Run Installer Commands ####################################################
+
+set -e
+
+echo "Running installer commands..."
+run_commands "$installer"
+
+#################################################################################################################################
