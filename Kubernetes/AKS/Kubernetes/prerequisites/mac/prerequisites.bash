bd5e6616fe8f13d19684753be35bd95d689c18e9
ShiranAvidov <shiran.avidov@logz.io>
2022-04-25 11:46:05 +0300

    fix

--- Kubernetes/AKS/Kubernetes/prerequisites/mac/prerequisites.bash
+++ Kubernetes/AKS/Kubernetes/prerequisites/mac/prerequisites.bash
@@ -5,7 +5,7 @@
 #################################################################################################################################
 
 # Load functions
-source ./functions.bash
+source ./prerequisites-functions.bash
 
 # Check if Kubectl is installed
 is_kubectl_installed
