a60f93a3b557e3204e86992d444606f2dd4b8c58
ShiranAvidov <shiran.avidov@logz.io>
2022-04-27 18:03:28 +0300

    installer

---
+++ Kubernetes/AKS/Kubernetes/telemetry/installer/mac/functions.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#################################################################################################################################
+################################################### Installer Mac Functions #####################################################
+#################################################################################################################################
+
+# Gets general params (params under datasource)
+function get_general_params () {
+    echo -e "general_params=$(jq -r '.configuration.subtypes[0].datasources[0].params' logzio-temp/app.json)" > logzio-temp/run_post_task
+}
+
+# Gets which products were selected (logs/metrics/tracing)
+function get_which_products_were_selected () {
+    #echo "Getting which products were selected..."
+    while read -r telemetry; do
+        local type=$(echo "$telemetry" | jq -r '.type')
+
+        if [ "$type" = "LOG_ANALYTICS" ]; then
+            echo -e "is_logs_option_selected=true" >> logzio-temp/run_post_task
+            echo -e "logs_params=$(echo \"$telemetry\" | jq -r '.params')" >> logzio-temp/run_post_task
+        elif [ "$type" = "METRICS" ]; then
+            echo -e "is_metrics_option_selected=true" >> logzio-temp/run_post_task
+            echo -e "metrics_params=$(echo \"$telemetry\" | jq -r '.params')" >> logzio-temp/run_post_task
+        elif [ "$type" = "TRACING" ]; then
+            echo -e "is_tracing_option_selected=true" >> logzio-temp/run_post_task
+            echo -e "tracing_params=$(echo \"$telemetry\" | jq -r '.params')" >> logzio-temp/run_post_task
+        fi
+    done < <(jq -c '.configuration.subtypes[0].datasources[0].telemetries[]' logzio-temp/app.json)
+}
---
+++ Kubernetes/AKS/Kubernetes/telemetry/installer/mac/installer.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#################################################################################################################################
+##################################################### Installer Mac Script ######################################################
+#################################################################################################################################
+
+# Load functions
+source ./logzio-temp/installer_functions.bash
+
+# Get general params
+execute_task "get_general_params" "getting general params"
+
+# Get which products were selected
+execute_task "get_which_products_were_selected" "getting which products were selected"
