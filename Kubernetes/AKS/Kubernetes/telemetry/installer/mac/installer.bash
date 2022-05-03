cef7d413cd8fd70f3b37b57cf5faa9f3afa8ba74
ShiranAvidov <shiran.avidov@logz.io>
2022-05-03 10:59:26 +0300

    installer

--- Kubernetes/AKS/Kubernetes/telemetry/installer/mac/installer.bash
+++ Kubernetes/AKS/Kubernetes/telemetry/installer/mac/installer.bash
@@ -10,9 +10,10 @@ source ./logzio-temp/installer_functions.bash
 # Get general params
 execute_task "get_general_params" "getting general params"
 
+: '
 # Get which products were selected
 execute_task "get_which_products_were_selected" "getting which products were selected"
-: '
+
 # Get tolerations helm sets
 execute_task "get_tolerations_helm_sets" "getting tolerations helm sets"
 
