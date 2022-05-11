a8cf2c2df5f199ff30a312d1ea127f5843482703
ShiranAvidov <shiran.avidov@logz.io>
2022-05-11 13:00:30 +0300

    prerequisites

--- Kubernetes/AKS/Kubernetes/prerequisites/mac/prerequisites.bash
+++ Kubernetes/AKS/Kubernetes/prerequisites/mac/prerequisites.bash
@@ -5,7 +5,7 @@
 #################################################################################################################################
 
 # Load functions
-echo -e "[INFO] Loading prerequisites functions script ..." >> logzio_agent.log
+echo -e "[INFO] Loading prerequisites functions ..." >> logzio_agent.log
 source ./logzio-temp/prerequisites_functions.bash
 
 # Check if kubectl is installed
