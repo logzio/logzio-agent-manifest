9250ec4c6cdaf5f7e5bbd2fc39f72e5e40ebe8d2
ShiranAvidov <shiran.avidov@logz.io>
2022-05-11 12:39:27 +0300

    prerequisites

--- Kubernetes/AKS/Kubernetes/prerequisites/mac/functions.bash
+++ Kubernetes/AKS/Kubernetes/prerequisites/mac/functions.bash
@@ -8,12 +8,13 @@
 # Error:
 #   Exit Code 1
 function is_kubectl_installed () {
+    echo -e "[INFO] Checking if kubectl is installed ..." >> logzio_agent.log
     which kubectl >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         return
     fi
 
-    echo -e "print_error \"prerequisites (1): kubectl is not installed\"" > logzio-temp/run
+    echo -e "print_error \"prerequisites.bash (1): kubectl is not installed\"" > logzio-temp/run
     return 1
 }
 
@@ -21,14 +22,17 @@ function is_kubectl_installed () {
 # Error:
 #   Exit Code 2
 function is_kubectl_connected_to_k8s_cluster () {
+    echo -e "[INFO] Checking if kubectl is connected to an active Kubernetes cluster ..." >> logzio_agent.log
     kubectl cluster-info > logzio-temp/task_result 2>&1
     if [ $? -eq 0 ]; then
+        cat logzio-temp/task_result >> logzio_agent.log
         return
     fi
 
-    local result=$(cat logzio-temp/task_result)
-    echo -e "echo -e \"$result\"" > logzio-temp/run
-    echo -e "print_error \"prerequisites (2): kubectl is not connected to an active Kubernetes cluster\"" >> logzio-temp/run
+    cat logzio-temp/task_result >> logzio_agent.log
+
+    echo -e "cat logzio-temp/task_result" > logzio-temp/run
+    echo -e "print_error \"prerequisites.bash (2): kubectl is not connected to an active Kubernetes cluster\"" >> logzio-temp/run
     return 2
 }
 
@@ -36,16 +40,19 @@ function is_kubectl_connected_to_k8s_cluster () {
 # Error:
 #   Exit Code 3
 function is_helm_installed () {
+    echo -e "[INFO] Checking if Helm is installed ..." >> logzio_agent.log
     which helm >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         return
     fi
 
+    echo -e "[INFO] Installing Helm ..." >> logzio_agent.log
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash > logzio-temp/task_result 2>&1
     if [ $? -ne 0 ]; then
-        local result=$(cat logzio-temp/task_result)
-        echo -e "echo -e \"$result\"" > logzio-temp/run
-        echo -e "print_error \"prerequisites (3): failed to install Helm\"" >> logzio-temp/run
+        cat logzio-temp/task_result >> logzio_agent.log
+
+        echo -e "cat logzio-temp/task_result" > logzio-temp/run
+        echo -e "print_error \"prerequisites.bash (3): failed to install Helm\"" >> logzio-temp/run
         return 3
     fi
 }
@@ -54,14 +61,16 @@ function is_helm_installed () {
 # Error:
 #   Exit Code 4
 function add_logzio_helm_repo () {
+    echo -e "[INFO] Adding Logz.io Helm repo ..." >> logzio_agent.log
     helm repo add logzio-helm https://logzio.github.io/logzio-helm > logzio-temp/task_result 2>&1
     if [ $? -eq 0 ]; then
         return
     fi
 
-    local result=$(cat logzio-temp/task_result)
-    echo -e "echo -e \"$result\"" > logzio-temp/run
-    echo -e "print_error \"prerequisites (4): failed to add Logz.io Helm repo\"" >> logzio-temp/run
+    cat logzio-temp/task_result >> logzio_agent.log
+
+    echo -e "cat logzio-temp/task_result" > logzio-temp/run
+    echo -e "print_error \"prerequisites.bash (4): failed to add Logz.io Helm repo\"" >> logzio-temp/run
     return 4
 }
 
@@ -69,13 +78,15 @@ function add_logzio_helm_repo () {
 # Error:
 #   Exit Code 5
 function update_logzio_helm_repo () {
+    echo -e "[INFO] Updating Logz.io Helm repo ..." >> logzio_agent.log
     helm repo update logzio-helm > logzio-temp/task_result 2>&1
     if [ $? -eq 0 ]; then
         return
     fi
 
-    local result=$(cat logzio-temp/task_result)
-    echo -e "echo -e \"$result\"" > logzio-temp/run
-    echo -e "print_error \"prerequisites (4): failed to update Logz.io Helm repo\"" >> logzio-temp/run
+    cat logzio-temp/task_result >> logzio_agent.log
+
+    echo -e "cat logzio-temp/task_result" > logzio-temp/run
+    echo -e "print_error \"prerequisites.bash (5): failed to update Logz.io Helm repo\"" >> logzio-temp/run
     return 5
 }
