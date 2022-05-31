1c049a8497e65e3e039788ef7f75c67153a22296
ShiranAvidov <shiran.avidov@logz.io>
2022-05-31 17:49:44 +0300

    agent

--- scripts/windows/functions.ps1
+++ scripts/windows/functions.ps1
@@ -135,7 +135,7 @@ function Install-Chocolatey {
     #if ($process.ExitCode -eq 0) {
     #    return
     #}
-    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression -Command (New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
     $local:result = Get-Content $using:taskResultFile
     $result = $result[0..($result.length-7)]
