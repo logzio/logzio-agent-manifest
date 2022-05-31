802ae133aa9e865fa4afa363d02c8b2a0c26590b
ShiranAvidov <shiran.avidov@logz.io>
2022-05-31 17:18:06 +0300

    agent

--- scripts/windows/functions.ps1
+++ scripts/windows/functions.ps1
@@ -130,15 +130,15 @@ function Install-Chocolatey {
     }
 
     Write-Log "INFO" "Installing Chocolatey ..."
-    try {
-        Set-ExecutionPolicy Bypass -Scope Process -Force
-        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-        iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1') | Out-Null)
+    $local:process = Start-Process powershell.exe -Argument "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression -Command (New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')" -RedirectStandardError $using:taskResultFile -PassThru -Wait
+    if ($process.ExitCode -eq 0) {
+        return
     }
-    catch {
-        Write-Run "Write-Error `"agent.ps1 (3): failed to install Chocolatey. $_`""
-        return 3
-    }
+
+    $local:result = Get-Content $using:taskResultFile
+    $result = $result[0..($result.length-7)]
+    Write-Run "Write-Error `"agent.ps1 (3): failed to install Chocolatey. $result`""
+    return 3
 }
 
 # Installs jq
