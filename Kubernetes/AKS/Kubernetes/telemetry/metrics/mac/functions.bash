425f6202c78f8fd4b390ba1b8d798c4dd38fc203
ShiranAvidov <shiran.avidov@logz.io>
2022-06-06 17:34:54 +0300

    metrics

--- Kubernetes/AKS/Kubernetes/telemetry/metrics/mac/functions.bash
+++ Kubernetes/AKS/Kubernetes/telemetry/metrics/mac/functions.bash
@@ -8,11 +8,11 @@
 # Output:
 #   helm_sets - Contains all the Helm sets
 function build_enable_metrics_helm_set () {
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Building enable metrics Helm set ..." >> logzio_agent.log
+    write_log "INFO" "Building enable metrics Helm set ..."
 
     local helm_set=" --set logzio-k8s-telemetry.metrics.enabled=true"
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] helm_set = $helm_set" >> logzio_agent.log
-    echo -e "helm_sets+='$helm_set'" > logzio-temp/run
+    write_log "INFO" "helm_set = $helm_set"
+    write_run "helm_sets+='$helm_set'"
 }
 
 # Builds Logz.io metrics listener URL Helm set
@@ -21,22 +21,22 @@ function build_enable_metrics_helm_set () {
 # Error:
 #   Exit Code 1
 function build_logzio_metrics_listener_url_helm_set () {
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Building Logz.io metrics listener URL Helm set ..." >> logzio_agent.log
+    write_log "INFO" "Building Logz.io metrics listener URL Helm set ..."
 
-    local listener_url=$(jq -r '.listenerUrl' logzio-temp/app.json)
+    local listener_url=$(jq -r '.listenerUrl' $app_json)
     if [[ "$listener_url" = null ]]; then
-        echo -e "print_error \"metrics.bash (1): '.listenerUrl' was not found in application JSON\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (1): '.listenerUrl' was not found in application JSON\""
         return 1
     fi
     if [[ -z "$listener_url" ]]; then
-        echo -e "print_error \"metrics.bash (1): '.listenerUrl' is empty in application JSON\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (1): '.listenerUrl' is empty in application JSON\""
         return 1
     fi
 
     listener_url="https://$listener_url:8053"
     local helm_set=" --set logzio-k8s-telemetry.secrets.ListenerHost=$listener_url"
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] helm_set = $helm_set" >> logzio_agent.log
-    echo -e "helm_sets+='$helm_set'" > logzio-temp/run
+    write_log "INFO" "helm_set = $helm_set"
+    write_run "helm_sets+='$helm_set'"
 }
 
 # Builds Logz.io metrics token Helm set
@@ -45,21 +45,21 @@ function build_logzio_metrics_listener_url_helm_set () {
 # Error:
 #   Exit Code 2
 function build_logzio_metrics_token_helm_set () {
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Building Logz.io metrics token Helm set ..." >> logzio_agent.log
+    write_log "INFO" "Building Logz.io metrics token Helm set ..."
 
-    local shipping_token=$(jq -r '.shippingTokens.METRICS' logzio-temp/app.json)
+    local shipping_token=$(jq -r '.shippingTokens.METRICS' $app_json)
     if [[ "$shipping_token" = null ]]; then
-        echo -e "print_error \"metrics.bash (2): '.shippingTokens.METRICS' was not found in application JSON\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (2): '.shippingTokens.METRICS' was not found in application JSON\""
         return 2
     fi
     if [[ -z "$shipping_token" ]]; then
-        echo -e "print_error \"metrics.bash (2): '.shippingTokens.METRICS' is empty in application JSON\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (2): '.shippingTokens.METRICS' is empty in application JSON\""
         return 2
     fi
     
     local helm_set=" --set logzio-k8s-telemetry.secrets.MetricsToken=$shipping_token"
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] helm_set = $helm_set" >> logzio_agent.log
-    echo -e "helm_sets+='$helm_set'" > logzio-temp/run
+    write_log "INFO" "helm_set = $helm_set"
+    write_run "helm_sets+='$helm_set'"
 }
 
 # Gets is Kubernetes runs on Windows OS
@@ -68,38 +68,38 @@ function build_logzio_metrics_token_helm_set () {
 # Error:
 #   Exit Code 3
 function get_is_k8s_runs_on_windows_os () {
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Getting if Kubernetes runs on Windows OS ..." >> logzio_agent.log
+    write_log "INFO" "Getting if Kubernetes runs on Windows OS ..."
 
     local is_windows_param=$(find_param "$metrics_params" "isWindows")
     if [[ -z "$is_windows_param" ]]; then
-        echo -e "print_error \"metrics.bash (3): isWindows param was not found\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (3): isWindows param was not found\""
         return 3
     fi
 
     local is_windows_value=$(echo -e "$is_windows_param" | jq -r '.value')
     if [[ "$is_windows_value" = null ]]; then
-        echo -e "print_error \"metrics.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=METRICS}].params[{name=isWindows}].value' was not found in application JSON\"" > logzio-temp/run
+        write_run "print_error \"metrics.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=METRICS}].params[{name=isWindows}].value' was not found in application JSON\""
         return 3
     fi
     if [[ -z "$is_windows_value" ]]; then
-        echo -e "print_error \"installer.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=METRICS}].params[{name=isWindows}].value' is empty in application JSON\"" > logzio-temp/run
+        write_run "print_error \"installer.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=METRICS}].params[{name=isWindows}].value' is empty in application JSON\""
         return 3
     fi
 
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] is_windows = $is_windows_value" >> logzio_agent.log
-    echo -e "is_windows=$is_windows_value" > logzio-temp/run
+    write_log "INFO" "is_windows = $is_windows_value"
+    write_run "is_windows=$is_windows_value"
 }
 
 # Builds Windows Node username and password Helms sets (if Kubernetes runs on Windows OS)
 # Output:
 #   helm_sets - Contains all the Helm sets
 function build_windows_node_username_and_password_helm_sets () {
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Building Windows Node username and password Helm sets ..." >> logzio_agent.log
+    write_log "INFO" "Building Windows Node username and password Helm sets ..."
 
-    echo -e "read -p \"\033[0;35mPlease enter your Windows Node username:\033[0;37m \" username" >> logzio-temp/run
-    echo -e "read -p \"\033[0;35mPlease enter your Windows Node password:\033[0;37m \" password" >> logzio-temp/run
-    echo -e "windows_sets+=\" --set logzio-k8s-telemetry.secrets.windowsNodeUsername=\$username\"" >> logzio-temp/run
-    echo -e "windows_sets+=\" --set logzio-k8s-telemetry.secrets.windowsNodePassword=\$password\"" >> logzio-temp/run
-    echo -e "echo -e \"[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] windows_sets = \$windows_sets\" >> logzio_agent.log" >> logzio-temp/run
-    echo -e "helm_sets+=\"\$windows_sets\"" >> logzio-temp/run
+    write_run "read -p \"\033[0;35mPlease enter your Windows Node username:\033[0;37m \" username"
+    write_run "read -p \"\033[0;35mPlease enter your Windows Node password:\033[0;37m \" password"
+    write_run "windows_sets+=\" --set logzio-k8s-telemetry.secrets.windowsNodeUsername=\$username\""
+    write_run "windows_sets+=\" --set logzio-k8s-telemetry.secrets.windowsNodePassword=\$password\""
+    write_run "echo -e \"[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] windows_sets = \$windows_sets\" >> logzio_agent.log"
+    write_run "helm_sets+=\"\$windows_sets\""
 }
