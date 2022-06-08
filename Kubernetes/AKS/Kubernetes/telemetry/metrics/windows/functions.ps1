8f93e3d161cf5261f669fed6cc1dedba74867de9
ShiranAvidov <shiran.avidov@logz.io>
2022-06-08 19:50:26 +0300

    metrics

--- Kubernetes/AKS/Kubernetes/telemetry/metrics/windows/functions.ps1
+++ Kubernetes/AKS/Kubernetes/telemetry/metrics/windows/functions.ps1
@@ -121,7 +121,7 @@ function Build-WindowsNodeUsernameAndPasswordHelmSets {
     Write-Run "`$local:password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR(`$securedPassword))"
     Write-Run "`$local:windowUsernameSet = `" --set logzio-k8s-telemetry.secrets.windowsNodeUsername=`$username`""
     Write-Run "`$local:windowsPasswordSet = `" --set logzio-k8s-telemetry.secrets.windowsNodePassword=`""
-    Write-Run "`$local:logPassword = `"`$password[0]*****`$password[`$password.Length-1]`""
+    Write-Run "`$local:logPassword = `"`$(password[0])*****`$(password[`$password.Length-1])`""
     Write-Run "Write-Log `"INFO`" `"windowsSets = `$windowUsernameSet`$windowsPasswordSet`$logPassword`""
     Write-Run "`$helmSets += `"`$windowUsernameSet`$windowsPasswordSet`$password`""
 }
