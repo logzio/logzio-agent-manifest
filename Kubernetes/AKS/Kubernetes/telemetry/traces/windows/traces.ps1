2368d832d7f78755dd760430132d92b414c9ad4c
ShiranAvidov <shiran.avidov@logz.io>
2022-06-09 11:27:14 +0300

    traces

---
+++ Kubernetes/AKS/Kubernetes/telemetry/traces/windows/traces.ps1
@@ -0,0 +1,16 @@
+#################################################################################################################################
+#################################################### Traces Windows Script ######################################################
+#################################################################################################################################
+
+# Load functions
+Write-Log "INFO" "Loading traces functions ..."
+. $logzioTempDir\traces_functions.ps1
+
+# Build enable traces Helm set
+Invoke-Task "Build-EnableTracesHelmSet" "building enable traces Helm set"
+
+# Build Logz.io traces token Helm set
+Invoke-Task "Build-LogzioTracesTokenHelmSet" "building Logz.io traces token Helm set"
+
+# Build Logz.io region Helm set
+Invoke-Task "Build-LogzioRegionHelmSet" "building Logz.io region Helm set"
