b80b8d13de5697dcfec60c9e6ab0d45102a81246
barakakoun <barak.kakoun@logz.io>
2022-06-13 16:30:16 +0200

    Fix

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const fs = require('fs')
 const path = require('path')
 
 const posts = fs.readdir(
-    path.join(process.env.GITHUB_WORKSPACE, 'content', 'posts')
+    path.join(process.env.GITHUB_WORKSPACE, 'manifest')
 )
 
 async function run() {
     console.log('Hello, world!');
+    console.log(posts);
 }
 
 run();
