e636e4fb9026dbf3ea651dfee371f557478420a1
ShiranAvidov <shiran.avidov@logz.io>
2022-07-24 13:23:37 +0300

    get log sources

--- localhost/mac/localhost/telemetry/logs/mac/functions.bash
+++ localhost/mac/localhost/telemetry/logs/mac/functions.bash
@@ -54,31 +54,67 @@ function get_logzio_logs_token () {
     write_run "logs_token=\"$shipping_token\""
 }
 
-# Adds logs receivers to OTEL config
+# Gets log sources
+# Output:
+#   log_sources - list of log sources
 # Error:
 #   Exit Code 3
+function get_log_sources_param () {
+    write_log "INFO" "Getting log_paths  ..."
+
+    local log_sources_param=$(find_param "$logs_params" "optional-log-sources")
+    if [[ -z "$log_sources_param" ]]; then
+        write_run "print_error \"logs.bash (3): log sources param was not found\""
+        return 3
+    fi
+
+    local log_sources_value=$(echo -e "$log_sources_param" | jq -c '.value[]')
+    if [[ "$log_sources_value" = null ]]; then
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=optional-log-sources}].value[]' was not found in application JSON\""
+        return 3
+    fi
+    if [[ -z "$log_sources_value" ]]; then
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=optional-log-sources}].value[]' is empty in application JSON\""
+        return 3
+    fi
+    
+    write_run "$log_sources=\"$log_sources_value\""
+}
+
+# Adds logs receivers to OTEL config
+# Error:
+#   Exit Code 4
 function add_logs_receivers_to_otel_config () {
     write_log "INFO" "Adding logs receivers to OTEL config ..."
 
     curl -fsSL $repo_path/telemetry/logs/logs_otel_receivers.yaml > $logzio_temp_dir/logs_otel_receivers.yaml 2>$task_error_file
     if [[ $? -ne 0 ]]; then
         local err=$(cat $task_error_file)
-        write_run "print_error \"logs.bash (3): failed to get logs_otel_receivers yaml file from logzio-agent-manifest repo.\n  $err\""
-        return 3
+        write_run "print_error \"logs.bash (4): failed to get logs_otel_receivers yaml file from logzio-agent-manifest repo.\n  $err\""
+        return 4
     fi
+
+    for log_source in $log_sources; do
+        yq e -i ".filelog.include += $log_source" $otel_config 2>$task_error_file
+        if [[ $? -ne 0 ]]; then
+            local err=$(cat $task_error_file)
+            write_run "print_error \"logs.bash (4): failed to add service pipeline logs exporter to OTEL config file.\n  $err\""
+            return 4
+        fi
+    done
 
     yq eval-all -i 'select(fileIndex==0).receivers += select(fileIndex==1) | select(fileIndex==0)' $otel_config $logzio_temp_dir/logs_otel_receivers.yaml 2>$task_error_file
     if [[ $? -ne 0 ]]; then
         local err=$(cat $task_error_file)
-        write_run "print_error \"logs.bash (3): failed to add logs receivers to OTEL config file.\n  $err\""
-        return 3
+        write_run "print_error \"logs.bash (4): failed to add logs receivers to OTEL config file.\n  $err\""
+        return 4
     fi
 
     yq e -i '.service.pipelines.logs.receivers += "filelog"' $otel_config 2>$task_error_file
     if [[ $? -ne 0 ]]; then
         local err=$(cat $task_error_file)
-        write_run "print_error \"logs.bash (3): failed to add service pipeline logs receiver to OTEL config file.\n  $err\""
-        return 3
+        write_run "print_error \"logs.bash (4): failed to add service pipeline logs receiver to OTEL config file.\n  $err\""
+        return 4
     fi
 }
 
