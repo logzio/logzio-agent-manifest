5ce373ae0eccd32509843b9f55a324b7b6139204
ShiranAvidov <shiran.avidov@logz.io>
2022-07-28 16:24:50 +0300

    plist is library

--- localhost/mac/localhost/telemetry/installer/mac/functions.bash
+++ localhost/mac/localhost/telemetry/installer/mac/functions.bash
@@ -164,9 +164,9 @@ function run_otelcol_contrib_as_a_service () {
         return 6
     fi
 
-    cat $logzio_temp_dir/com.logzio.OTELCollector.plist > ./com.logzio.OTELCollector.plist
+    write_run "sudo cat $logzio_temp_dir/com.logzio.OTELCollector.plist > /Library/LaunchAgents/com.logzio.OTELCollector.plist"
 
-    launchctl load ./com.logzio.OTELCollector.plist >/dev/null 2>$task_error_file
+    launchctl load $logzio_temp_dir/com.logzio.OTELCollector.plist >/dev/null 2>$task_error_file
     local err=$(cat $task_error_file)
     if [[ ! -z "$err" ]]; then
         write_run "print_error \"installer.bash (6): failed to load OTEL collector plist file.\n  $err\""
@@ -179,7 +179,7 @@ function run_otelcol_contrib_as_a_service () {
     fi
 
     status=$(launchctl list | grep com.logzio.OTELCollector | grep -oe '[0-9]\+')
-    launchctl unload ./com.logzio.OTELCollector.plist >/dev/null 2>&1
+    launchctl unload $logzio_temp_dir/com.logzio.OTELCollector.plist >/dev/null 2>&1
     write_run "print_error \"installer.bash (6): failed to run OTEL collector plist file (status $status).\n  $err\""
     return 6
 }
