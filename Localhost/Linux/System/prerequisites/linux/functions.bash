791e5b0a1eb2358b860f0aa6ab20e360e1a2198d
ShiranAvidov <shiran.avidov@logz.io>
2022-08-30 15:59:50 +0300

    fix

--- Localhost/Linux/System/prerequisites/linux/functions.bash
+++ Localhost/Linux/System/prerequisites/linux/functions.bash
@@ -23,14 +23,33 @@ function install_yq () {
     write_run "yq_bin=\"$yq_bin\""
 }
 
+# Installs nc
+# Output:
+#   nc_bin - The nc binary file path
+# Error:
+#   Exit Code 2
+function install_nc () {
+    write_log "INFO" "Installing nc ..."
+    curl -fsSL https://github.com/mikefarah/yq/releases/download/v4.27.2/yq_linux_amd64.tar.gz > $logzio_temp_dir/yq.tar.gz 2>$task_error_file
+    if [[ $? -ne 0 ]]; then
+        local err=$(cat $task_error_file)
+        write_run "print_error \"prerequisites.bash (1): failed to get yq binary file from Github.\n  $err\""
+        return 1
+    fi
+
+    yq_bin="$logzio_temp_dir/yq_linux_amd64"
+    tar -zxf $logzio_temp_dir/yq.tar.gz --directory $logzio_temp_dir *_amd64
+    write_run "yq_bin=\"$yq_bin\""
+}
+
 # Checks if localhost can connect to Logz.io logs (port 8071)
 # Error:
 #   Exit Code 2
 function can_localhost_connect_to_logzio_logs () {
     write_log "INFO" "Checking if localhost can connect to Logz.io logs (port 8071) ..."
 
-    local result=$(nc -v -G 2 -w 2 -z listener.logz.io 8071 2>&1 | grep succeeded)
-    if [[ ! -z "$result" ]]; then
+    wget -q -t 3 --timeout=5 listener.logz.io:8071
+    if [[ $? -eq 8 ]]; then
         return
     fi
 
@@ -44,8 +63,8 @@ function can_localhost_connect_to_logzio_logs () {
 function can_localhost_connect_to_logzio_metrics () {
     write_log "INFO" "Checking if localhost can connect to Logz.io metrics (port 8053) ..."
 
-    local result=$(nc -v -G 2 -w 2 -z listener.logz.io 8053 2>&1 | grep succeeded)
-    if [[ ! -z "$result" ]]; then
+    wget -q -t 3 --timeout=5 listener.logz.io:8053
+    if [[ $? -eq 8 ]]; then
         return
     fi
 
