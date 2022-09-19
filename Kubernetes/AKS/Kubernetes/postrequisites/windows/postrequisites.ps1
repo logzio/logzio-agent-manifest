65f7f760c92e2bacfe5400a3392147bc65e969b4
ShiranAvidov <shiran.avidov@logz.io>
2022-09-19 11:25:02 +0300

    fix

--- Kubernetes/AKS/Kubernetes/postrequisites/windows/postrequisites.ps1
+++ Kubernetes/AKS/Kubernetes/postrequisites/windows/postrequisites.ps1
@@ -7,13 +7,13 @@ Write-Log "INFO" "Loading postrequisites functions ..."
 . $logzioTempDir\postrequisites_functions.ps1
 
 # Check if all pods are running or completed
-Invoke-Task "are_all_pods_running_or_completed" "checking if all pods are running or completed"
+Invoke-Task "Test-AreAllPodsRunningOrCompleted" "checking if all pods are running or completed"
 
 if ($areAllPodsRunningOrCompleted) {
     # Check if any pod is pending
-    Invoke-Task "is_any_pod_pending" "checking if any pod is pending"
+    Invoke-Task "Test-IsAnyPodPending" "checking if any pod is pending"
     # Check if any pod is failed
-    Invoke-Task "is_any_pod_failed" "checking if any pod is failed"
+    Invoke-Task "Test-IsAnyPodFailed" "checking if any pod is failed"
 }
 
 # Finished successfully
