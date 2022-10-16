3d2f9484123a19542746bf3848d115b2bf8c4424
ShiranAvidov <shiran.avidov@logz.io>
2022-10-16 10:33:44 +0300

    localhost prerequisites

--- datasources/localhost/windows/prerequisites/functions.ps1
+++ datasources/localhost/windows/prerequisites/functions.ps1
@@ -1,5 +1,5 @@
 #################################################################################################################################
-############################################### WINDOWS Prerequisites Functions #################################################
+############################################# WINDOWS Subtype Prerequisites Script ##############################################
 #################################################################################################################################
 
 # Checks if localhost can connect to Logz.io logs (port 8071)
@@ -15,9 +15,9 @@ function Test-CanLocalhostConnectToLogzioLogs {
     Send-LogToLogzio $LogLevelDebug $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
     Write-Log $LogLevelDebug $Message
 
-    $local:LogzioListenerUrl = &$JqExe -r .listenerUrl $AppJson
+    $local:LogzioListenerUrl = &$JqExe -r .listenerUrl $AppJson 2>$TaskErrorFile
     if ($LASTEXITCODE -ne 0) {
-        $Message = "prerequisites.ps1 ($ExitCode): error getting listener url from application json: $_"
+        $Message = "prerequisites.ps1 ($ExitCode): error getting listener url from application json: $(Get-Content -Path $TaskErrorFile)"
         Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
         Write-TaskPostRun "Write-Error '$Message'"
 
@@ -31,14 +31,14 @@ function Test-CanLocalhostConnectToLogzioLogs {
         }
 
         $Message = "prerequisites.ps1 ($ExitCode): localhost cannot connect to '$LogzioListenerUrl`:8071'. please check your network for port 8071"
-        Send-LogToLogzio $LogLevelError $Message $LogStepPreInit $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
-        Write-Log $LogLevelError $Message
+        Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
+        Write-TaskPostRun "Write-Error '$Message'"
 
     }
     catch {
         $Message = "prerequisites.ps1 ($ExitCode): error testing network connection to '$LogzioListenerUrl`:8071': $_"
-        Send-LogToLogzio $LogLevelError $Message $LogStepPreInit $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
-        Write-Log $LogLevelError $Message
+        Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
+        Write-TaskPostRun "Write-Error '$Message'"
 
         return $ExitCode
     }
@@ -57,9 +57,9 @@ function Test-CanLocalhostConnectToLogzioMetrics {
     Send-LogToLogzio $LogLevelDebug $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
     Write-Log $LogLevelDebug $Message
 
-    $local:LogzioListenerUrl = &$JqExe -r .listenerUrl $AppJson
+    $local:LogzioListenerUrl = &$JqExe -r .listenerUrl $AppJson 2>$TaskErrorFile
     if ($LASTEXITCODE -ne 0) {
-        $Message = "prerequisites.ps1 ($ExitCode): error getting listener url from application json: $_"
+        $Message = "prerequisites.ps1 ($ExitCode): error getting listener url from application json: $(Get-Content -Path $TaskErrorFile)"
         Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptPrerequisites $FuncName $AgentId $Platfrom $Subtype
         Write-TaskPostRun "Write-Error '$Message'"
 
@@ -73,14 +73,14 @@ function Test-CanLocalhostConnectToLogzioMetrics {
         }
 
         $Message = "prerequisites.ps1 ($ExitCode): localhost cannot connect to '$LogzioListenerUrl`:8053'. please check your network for port 8053"
-        Send-LogToLogzio $LogLevelError $Message $LogStepPreInit $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
-        Write-Log $LogLevelError $Message
+        Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
+        Write-TaskPostRun "Write-Error '$Message'"
 
     }
     catch {
         $Message = "prerequisites.ps1 ($ExitCode): error testing network connection to '$LogzioListenerUrl`:8053': $_"
-        Send-LogToLogzio $LogLevelError $Message $LogStepPreInit $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
-        Write-Log $LogLevelError $Message
+        Send-LogToLogzio $LogLevelError $Message $LogStepPrerequisites $LogScriptAgent $FuncName $AgentId $Platfrom $Subtype
+        Write-TaskPostRun "Write-Error '$Message'"
 
         return $ExitCode
     }
--- datasources/localhost/windows/prerequisites/prerequisites.ps1
+++ datasources/localhost/windows/prerequisites/prerequisites.ps1
@@ -1,10 +1,17 @@
 #################################################################################################################################
-################################################# WINDOWS Prerequisites Script ##################################################
+############################################# WINDOWS Subtype Prerequisites Script ##############################################
 #################################################################################################################################
+
+# Print title
+Write-Host
+Write-Host '#####################'
+Write-Host '### ' -NoNewline
+Write-Host 'Prerequisites' -ForegroundColor Magenta -NoNewline
+Write-Host ' ###'
+Write-Host '#####################'
 
 # Check if localhost can connect to Logz.io logs (port 8071)
 Invoke-Task 'Test-CanLocalhostConnectToLogzioLogs' @{} 'Checking if localhost can connect to Logz.io logs' @("$LogzioTempDir\$Platform\$SubType\prerequisites\functions.ps1")
-
 # Check if localhost can connect to Logz.io metrics (port 8053)
 Invoke-Task 'Test-CanLocalhostConnectToLogzioMetrics' @{} 'Checking if localhost can connect to Logz.io metrics' @("$LogzioTempDir\$Platform\$SubType\prerequisites\functions.ps1")
 
