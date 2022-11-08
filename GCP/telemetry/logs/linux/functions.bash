003885e33478cce3f39cd2b98eba647346d8acd5
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-11-08 15:44:38 +0200

    update log filter input add proper functions

--- GCP/telemetry/logs/linux/functions.bash
+++ GCP/telemetry/logs/linux/functions.bash
@@ -49,9 +49,9 @@ function get_logzio_logs_token () {
     write_run "logs_token=\"$shipping_token\""
 }
 
-# Gets log sources
+# Gets Function Name
 # Output:
-#   log_sources - list of log sources
+#   function_name - function name
 # Error:
 #   Exit Code 3
 function get_function_name () {
@@ -59,17 +59,17 @@ function get_function_name () {
 
     local function_name_param=$(find_param "$logs_params" "functionName")
     if [[ -z "$function_name_param" ]]; then
-        write_run "print_error \"logs.bash (3): log sources param was not found\""
+        write_run "print_error \"logs.bash (3): function name param was not found\""
         return 3
     fi
 
-    local function_name_value=$(echo -e "$function_name_param" | jq -c '.value[]')
+    local function_name_value=$(echo -e "$function_name_param" | jq -c '.value')
     if [[ "$function_name_value" = null ]]; then
-        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=functionName}].value[]' was not found in application JSON\""
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=functionName}].value' was not found in application JSON\""
         return 3
     fi
     if [[ -z "$function_name_value" ]]; then
-        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=functionName}].value[]' is empty in application JSON\""
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=functionName}].value' is empty in application JSON\""
         return 3
     fi
     
@@ -77,36 +77,64 @@ function get_function_name () {
     write_run "function_name=\"$function_name_value\""
 }
 
+# Gets Function Name
+# Output:
+#   filter_log - function name
+# Error:
+#   Exit Code 3
+function get_filter_log () {
+    write_log "INFO" "Getting log sources ..."
+
+    local filter_log_param=$(find_param "$logs_params" "filterLog")
+    if [[ -z "$filter_log_param" ]]; then
+        write_run "print_error \"logs.bash (3): filters log param was not found\""
+        return 3
+    fi
+
+    local filter_log_value=$(echo -e "$ffilter_log_param" | jq -c '.value')
+    if [[ "$function_name_value" = null ]]; then
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=filterLog}].value' was not found in application JSON\""
+        return 3
+    fi
+    if [[ -z "$filter_log_value" ]]; then
+        write_run "print_error \"logs.bash (3): '.configuration.subtypes[0].datasources[0].telemetries[{type=LOG_ANALYTICS}].params[{name=filterLog}].value' is empty in application JSON\""
+        return 3
+    fi
+    
+    write_log "INFO" "filter_log = $filter_log_value"
+    write_run "filter_log=\"$filter_log_value\""
+}
+
+
 
 function populate_data_to_json (){
     write_log "[INFO] Сreate build file..."
-    curl -fsSL https://github.com/logzio/otel-collector-distro/releases/download/v0.56.1/otelcol-logzio-linux_amd64.tar.gz > $logzio_temp_dir/otelcol-logzio.tar.gz 2>$task_error_file
+    curl -fsSL https://github.com/logzio/logzio-google-pubsub/archive/refs/tags/v1.0.0.tar.gz > $logzio_temp_dir/otelcol-logzio.tar.gz 2>$task_error_file
 
-    contents="$(jq --arg token "${token}" '.substitutions._LOGZIO_TOKEN = $token' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq  --arg type_of_log "${type}" '.substitutions._TYPE_NAME = $type_of_log' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg region "${region}" '.substitutions._REGION = $region' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg listener_url "${listener_url}" '.substitutions._LOGZIO_LISTENER = $listener_url' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg function_name "${function_name}" '.substitutions._FUNCTION_NAME = $function_name+"-func_logzio"' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg topic_prefix "${function_name}" '.substitutions._PUBSUB_TOPIC_NAME = $topic_prefix+"-pubsub-topic-logs-to-logzio"' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg subscription_prefix "${function_name}" '.substitutions._PUBSUB_SUBSCRIPTION_NAME = $subscription_prefix+"-pubsub-subscription-logs-to-logzio"' config.json)"
-    echo "${contents}" > config.json
-    contents="$(jq --arg sink_prefix "${function_name}" '.substitutions._SINK_NAME = $sink_prefix+"-sink-logs-to-logzio"' config.json)"
-    echo "${contents}" > config.json
+    contents="$(jq --arg token "${token}" '.substitutions._LOGZIO_TOKEN = $token'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq  --arg type_of_log "${type}" '.substitutions._TYPE_NAME = $type_of_log'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg region "${region}" '.substitutions._REGION = $region'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg listener_url "${listener_url}" '.substitutions._LOGZIO_LISTENER = $listener_url'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg function_name "${function_name}" '.substitutions._FUNCTION_NAME = $function_name+"-func_logzio"'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg topic_prefix "${function_name}" '.substitutions._PUBSUB_TOPIC_NAME = $topic_prefix+"-pubsub-topic-logs-to-logzio"'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg subscription_prefix "${function_name}" '.substitutions._PUBSUB_SUBSCRIPTION_NAME = $subscription_prefix+"-pubsub-subscription-logs-to-logzio"'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
+    contents="$(jq --arg sink_prefix "${function_name}" '.substitutions._SINK_NAME = $sink_prefix+"-sink-logs-to-logzio"'  $logzio_temp_dir/config.json)"
+    echo "${contents}" >  $logzio_temp_dir/config.json
     contents="$(jq --arg filter_log "${filter_log}" '.substitutions._FILTER_LOG = $filter_log' config.json)"
-    echo "${contents}" > config.json
+    echo "${contents}" >  $logzio_temp_dir/config.json
 
     write_log "[INFO] Populate data to json finished."
 }
 
 function run_cloud_build(){
-    
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Initialize Cloud Build ..."
+    write_log "[INFO] Initialize Cloud Build ..."
     # Take project ID and project Number
     project_number="$(gcloud projects list \
     --filter="$(gcloud config get-value project)" \
@@ -124,8 +152,8 @@ function run_cloud_build(){
     #Get Access Token for upload
     access_token="$(gcloud config config-helper --format='value(credential.access_token)')"
     # Run project
-    cmd_create_cloud_build="$(curl -X POST -T config.json -H "Authorization: Bearer $access_token" https://cloudbuild.googleapis.com/v1/projects/$project_id/builds)"
+    cmd_create_cloud_build="$(curl -X POST -T $logzio_temp_dir/config.json -H "Authorization: Bearer $access_token" https://cloudbuild.googleapis.com/v1/projects/$project_id/builds)"
 
-    echo "$cmd_create_cloud_build"
-    echo -e "[INFO] [$(date +"%Y-%m-%d %H:%M:%S")] Cloud Build Initialization is finished."
+    write_log "$cmd_create_cloud_build"
+    write_log "[INFO] Cloud Build Initialization is finished."
 }
--- GCP/telemetry/logs/linux/logs.bash
+++ GCP/telemetry/logs/linux/logs.bash
@@ -18,6 +18,10 @@ execute_task "get_logzio_logs_token" "getting Logz.io logs token"
 # Get log sources
 execute_task "get_google_cloud_fuction_name" "getting google cloud function name"
 
+# Get log sources
+execute_task "get_filter_log" "getting google cloud logging filter"
+
+
 # Build configuration file
 execute_task "populate_data_to_config" "add data to config"
 
