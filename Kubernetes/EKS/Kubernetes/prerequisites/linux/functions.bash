5f30a88a95e45229755c3baf201ce023883c74c4
ShiranAvidov <shiran.avidov@logz.io>
2022-11-27 11:42:58 +0200

    fix

--- Kubernetes/EKS/Kubernetes/prerequisites/linux/functions.bash
+++ Kubernetes/EKS/Kubernetes/prerequisites/linux/functions.bash
@@ -77,7 +77,7 @@ function can_k8s_cluster_connect_to_logzio_logs () {
     local retries = 3
     while [[ $retries -ne 0 ]]; do
         local pod_status=$(kubectl get pods | grep logzio-metrics-connection-test | tr -s ' ' | cut -d ' ' -f3)
-        if [[ "$pod_status" == "Completed" ]]; then
+        if [[ "$pod_status" = "Completed" ]]; then
             is_pod_completed=true
             break
         fi
@@ -143,7 +143,7 @@ function can_k8s_cluster_connect_to_logzio_metrics () {
     local retries = 3
     while [[ $retries -ne 0 ]]; do
         local pod_status=$(kubectl get pods | grep logzio-metrics-connection-test | tr -s ' ' | cut -d ' ' -f3)
-        if [[ "$pod_status" == "Completed" ]]; then
+        if [[ "$pod_status" = "Completed" ]]; then
             is_pod_completed=true
             break
         fi
