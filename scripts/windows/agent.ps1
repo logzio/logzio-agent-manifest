fd1f6c7685f16042e0e00145796c23ba0357195d
ShiranAvidov <48657224+ShiranAvidov@users.noreply.github.com>
2022-12-12 10:55:30 +0200

    Themes (#61)
    * themes
    
    * themes
    
    * themes
    
    * themes
    
    * linux
    
    * mac
    
    * windows
    
    * windows version
    
    * fix
    
    * fix

--- scripts/windows/agent.ps1
+++ scripts/windows/agent.ps1
@@ -143,7 +143,7 @@ function Write-AgentSupport {
 
 
 # Agent version
-$script:AgentVersion = 'v1.0.32'
+$script:AgentVersion = 'v1.0.33'
 
 # Settings
 $ProgressPreference = 'SilentlyContinue'
@@ -159,32 +159,17 @@ $script:IsPostrequisiteFailed = $false
 $script:IsAgentCompleted = $false
 
 # Print main title
-Write-Host "
-    LLLLLLLLLLL                                                                             iiii                   
-    L:::::::::L                                                                            i::::i                  
-    L:::::::::L                                                                             iiii                   
-    LL:::::::LL                                                                                                    
-      L:::::L                  ooooooooooo      ggggggggg   gggggzzzzzzzzzzzzzzzzz        iiiiiii    ooooooooooo   
-      L:::::L                oo:::::::::::oo   g:::::::::ggg::::gz:::::::::::::::z        i:::::i  oo:::::::::::oo 
-      L:::::L               o:::::::::::::::o g:::::::::::::::::gz::::::::::::::z          i::::i o:::::::::::::::o
-      L:::::L               o:::::ooooo:::::og::::::ggggg::::::ggzzzzzzzz::::::z           i::::i o:::::ooooo:::::o
-      L:::::L               o::::o     o::::og:::::g     g:::::g       z::::::z            i::::i o::::o     o::::o
-      L:::::L               o::::o     o::::og:::::g     g:::::g      z::::::z             i::::i o::::o     o::::o
-      L:::::L               o::::o     o::::og:::::g     g:::::g     z::::::z              i::::i o::::o     o::::o
-      L:::::L         LLLLLLo::::o     o::::og::::::g    g:::::g    z::::::z               i::::i o::::o     o::::o
-    LL:::::::LLLLLLLLL:::::Lo:::::ooooo:::::og:::::::ggggg:::::g   z::::::zzzzzzzz        i::::::io:::::ooooo:::::o
-    L::::::::::::::::::::::Lo:::::::::::::::o g::::::::::::::::g  z::::::::::::::z ...... i::::::io:::::::::::::::o
-    L::::::::::::::::::::::L oo:::::::::::oo   gg::::::::::::::g z:::::::::::::::z .::::. i::::::i oo:::::::::::oo 
-    LLLLLLLLLLLLLLLLLLLLLLLL   ooooooooooo       gggggggg::::::g zzzzzzzzzzzzzzzzz ...... iiiiiiii   ooooooooooo   
-                                                         g:::::g                                                   
-                                             gggggg      g:::::g                                                   
-                                             g:::::gg   gg:::::g               Agent $script:AgentVersion                                   
-                                              g::::::ggg:::::::g                                                   
-                                               gg:::::::::::::g                                                    
-                                                 ggg::::::ggg                                                      
-                                                    gggggg                                                          
-" -ForegroundColor Cyan
-Write-Host
+try {
+    # Christmas theme
+    . $env:TEMP\Logzio\logo-themes\christmas.ps1 -ErrorAction Stop
+}
+catch {
+    Write-Host $_
+    Write-Host
+    Write-Host "Logz.io Agent $AgentVersion" -ForegroundColor Cyan
+    Write-Host
+    Write-Host
+}
 
 try {
     # Load agent scripts
---
+++ scripts/windows/logo-themes/hanukkah.ps1
@@ -0,0 +1,143 @@
+#################################################################################################################################
+################################################## WINDOWS Logo Theme Script ####################################################
+#################################################################################################################################
+
+# Hanukkah title
+
+Write-Host
+Write-Host '         (' -ForegroundColor Yellow
+Write-Host '         )\' -ForegroundColor Yellow
+Write-Host '        /  )' -ForegroundColor Yellow
+Write-Host '       (' -ForegroundColor Yellow -NoNewline
+Write-Host ' *' -ForegroundColor Red -NoNewline
+Write-Host ' (' -ForegroundColor Yellow
+Write-Host '        \' -ForegroundColor Cyan -NoNewline
+Write-Host '#' -ForegroundColor Red -NoNewline
+Write-Host '/' -ForegroundColor Cyan -NoNewline
+Write-Host '                         )               )                    (                   (            )' -ForegroundColor Yellow
+Write-Host '         #' -ForegroundColor White -NoNewline
+Write-Host '                         /(              /(                    )\                  )\          /(' -ForegroundColor Yellow
+Write-Host '    LLLLLLLLLLL' -ForegroundColor Yellow -NoNewLine
+Write-Host '                    ( \            (  \                  / )                 /  )         ( \' -ForegroundColor Yellow
+Write-Host '    L:::::::::L' -ForegroundColor Yellow -NoNewLine
+Write-Host '                   )' -ForegroundColor Yellow -NoNewline
+Write-Host ' *' -ForegroundColor Red -NoNewline
+Write-Host ' )' -ForegroundColor Yellow -NoNewline
+Write-Host '           )' -ForegroundColor Yellow -NoNewline
+Write-Host ' *' -ForegroundColor Red -NoNewline
+Write-Host ' )' -ForegroundColor Yellow -NoNewline
+Write-Host '                (' -ForegroundColor Yellow -NoNewline
+Write-Host ' *' -ForegroundColor Red -NoNewline
+Write-Host ' (' -ForegroundColor Yellow -NoNewline
+Write-Host '               (' -ForegroundColor Yellow -NoNewLine
+Write-Host ' *' -ForegroundColor Red -NoNewLine
+Write-Host ' (' -ForegroundColor Yellow -NoNewline
+Write-Host '        )' -ForegroundColor Yellow -NoNewline
+Write-Host ' *' -ForegroundColor Red -NoNewline
+Write-Host ' )' -ForegroundColor Yellow
+Write-Host '    L:::::::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '                   \' -ForegroundColor Cyan -NoNewline
+Write-Host ' #' -ForegroundColor Red -NoNewline
+Write-Host ' /' -ForegroundColor Cyan -NoNewline
+Write-Host '            \' -ForegroundColor Cyan -NoNewline
+Write-Host '#' -ForegroundColor Red -NoNewline
+Write-Host '/' -ForegroundColor Cyan -NoNewline
+Write-Host '                 \' -ForegroundColor Cyan -NoNewline
+Write-Host ' #' -ForegroundColor Red -NoNewline
+Write-Host ' /' -ForegroundColor Cyan -NoNewline
+Write-Host '                \' -ForegroundColor Cyan -NoNewLine
+Write-Host '#' -ForegroundColor Red -NoNewLine
+Write-Host '/' -ForegroundColor Cyan -NoNewline
+Write-Host '         \' -ForegroundColor Cyan -NoNewline
+Write-Host ' #' -ForegroundColor Red -NoNewline
+Write-Host ' /' -ForegroundColor Cyan
+Write-Host '    LL:::::::LL' -ForegroundColor Yellow -NoNewline
+Write-Host '                     #' -ForegroundColor White -NoNewline
+Write-Host '               #' -ForegroundColor White -NoNewline
+Write-Host '                    #' -ForegroundColor White -NoNewline
+Write-Host '                   #' -ForegroundColor White -NoNewline
+Write-Host '            #' -ForegroundColor White
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '                  ooooooooooo' -ForegroundColor Cyan -NoNewline
+Write-Host '      ggggggggg   ggggg' -ForegroundColor Yellow -NoNewline
+Write-Host 'zzzzzzzzzzzzzzzzz' -ForegroundColor Cyan -NoNewline
+Write-Host '        iiiiiii' -ForegroundColor Yellow -NoNewline
+Write-Host '    ooooooooooo' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '                oo:::::::::::oo' -ForegroundColor Cyan -NoNewline
+Write-Host '   g:::::::::ggg::::g' -ForegroundColor Yellow -NoNewline
+Write-Host 'z:::::::::::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '        i:::::i' -ForegroundColor Yellow -NoNewline
+Write-Host '  oo:::::::::::oo' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '               o:::::::::::::::o' -ForegroundColor Cyan -NoNewline
+Write-Host ' g:::::::::::::::::g' -ForegroundColor Yellow -NoNewline
+Write-Host 'z::::::::::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '          i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o:::::::::::::::o' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '               o:::::ooooo:::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g::::::ggggg::::::gg' -ForegroundColor Yellow -NoNewline
+Write-Host 'zzzzzzzz::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '           i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o:::::ooooo:::::o' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '               o::::o     o::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g:::::g     g:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '       z::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '            i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o::::o     o::::o' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '               o::::o     o::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g:::::g     g:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '      z::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '             i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o::::o     o::::o' -ForegroundColor Cyan
+Write-Host '      L:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host '               o::::o     o::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g:::::g     g:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '     z::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '              i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o::::o     o::::o' -ForegroundColor Cyan
+Write-Host '      L:::::L         LLLLLL' -ForegroundColor Yellow -NoNewline
+Write-Host 'o::::o     o::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g::::::g    g:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '    z::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host '               i::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' o::::o     o::::o' -ForegroundColor Cyan
+Write-Host '    LL:::::::LLLLLLLLL:::::L' -ForegroundColor Yellow -NoNewline
+Write-Host 'o:::::ooooo:::::o' -ForegroundColor Cyan -NoNewline
+Write-Host 'g:::::::ggggg:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '   z::::::zzzzzzzz' -ForegroundColor Cyan -NoNewline
+Write-Host '        i::::::i' -ForegroundColor Yellow -NoNewline
+Write-Host 'o:::::ooooo:::::o' -ForegroundColor Cyan
+Write-Host '    L::::::::::::::::::::::L' -ForegroundColor Yellow -NoNewline
+Write-Host 'o:::::::::::::::o' -ForegroundColor Cyan -NoNewline
+Write-Host ' g::::::::::::::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '  z::::::::::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host ' ......' -ForegroundColor White -NoNewline
+Write-Host ' i::::::i' -ForegroundColor Yellow -NoNewline
+Write-Host 'o:::::::::::::::o' -ForegroundColor Cyan
+Write-Host '    L::::::::::::::::::::::L' -ForegroundColor Yellow -NoNewline
+Write-Host ' oo:::::::::::oo' -ForegroundColor Cyan -NoNewline
+Write-Host '   gg::::::::::::::g' -ForegroundColor Yellow -NoNewline
+Write-Host ' z:::::::::::::::z' -ForegroundColor Cyan -NoNewline
+Write-Host ' .::::.' -ForegroundColor White -NoNewline
+Write-Host ' i::::::i' -ForegroundColor Yellow -NoNewline
+Write-Host ' oo:::::::::::oo' -ForegroundColor Cyan
+Write-Host '    LLLLLLLLLLLLLLLLLLLLLLLL' -ForegroundColor Yellow -NoNewline
+Write-Host '   ooooooooooo' -ForegroundColor Cyan -NoNewline
+Write-Host '       gggggggg::::::g' -ForegroundColor Yellow -NoNewline
+Write-Host ' zzzzzzzzzzzzzzzzz' -ForegroundColor Cyan -NoNewline
+Write-Host ' ......' -ForegroundColor White -NoNewline
+Write-Host ' iiiiiiii' -ForegroundColor Yellow -NoNewline
+Write-Host '   ooooooooooo' -ForegroundColor Cyan
+Write-Host '                                                         g:::::g' -ForegroundColor Yellow
+Write-Host '                                             gggggg      g:::::g' -ForegroundColor Yellow
+Write-Host '                                             g:::::gg   gg:::::g' -ForegroundColor Yellow -NoNewline
+Write-Host '               Agent $script:AgentVersion' -ForegroundColor Cyan
+Write-Host '                                              g::::::ggg:::::::g' -ForegroundColor Yellow
+Write-Host '                                               gg:::::::::::::g' -ForegroundColor Yellow
+Write-Host '                                                 ggg::::::ggg' -ForegroundColor Yellow
+Write-Host '                                                    gggggg' -ForegroundColor Yellow
+Write-Host
