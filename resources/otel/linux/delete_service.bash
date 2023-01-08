c8715d62e2b0ea1cdc0a9feb525320cff6283b54
ShiranAvidov <shiran.avidov@logz.io>
2023-01-08 10:46:33 +0200

    resources

---
+++ resources/otel/linux/delete_service.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#################################################################################################################################
+#################################################### LINUX DELETE SERVICE #######################################################
+#################################################################################################################################
+
+systemctl stop "LOGZIO_OTEL_COLLECTOR_SERVICE_NAME"
+if [[ $? -ne 0 ]]; then
+    exit 1
+fi
+
+systemctl disable "LOGZIO_OTEL_COLLECTOR_SERVICE_NAME"
+if [[ $? -ne 0 ]]; then
+    exit 2
+fi
+
+rm "/etc/systemd/system/LOGZIO_OTEL_COLLECTOR_SERVICE_NAME.service" 2>/dev/null
+rm "/usr/lib/systemd/system/LOGZIO_OTEL_COLLECTOR_SERVICE_NAME.service" 2>/dev/null
+rm "/etc/init.d/LOGZIO_OTEL_COLLECTOR_SERVICE_NAME" 2>/dev/null
