2940376f6071142afc1c0071b6cb4ba88a91573c
ShiranAvidov <shiran.avidov@logz.io>
2023-01-10 11:56:40 +0200

    prerequisites

--- datasources/linux/aws/ec2/prerequisites/prerequisites.bash
+++ datasources/linux/aws/ec2/prerequisites/prerequisites.bash
@@ -11,7 +11,7 @@ echo -e "###$PURPLE_COLOR Prerequisites $WHITE_COLOR###"
 echo -e '#####################'
 
 load_prerequisites_utils
-MACHINE_NAME='localhost'
+MACHINE_NAME='EC2'
 # Check if localhost can connect to Logz.io logs (port 8071)
 execute_task 'can_machine_connect_to_logzio_logs' 'Checking if localhost can connect to Logz.io logs'
 # Check if localhost can connect to Logz.io metrics (port 8053)
