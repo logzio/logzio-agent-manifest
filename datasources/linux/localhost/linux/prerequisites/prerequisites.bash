8b6e17461ba0395af2f43be1feb9a65196dcd084
ShiranAvidov <shiran.avidov@logz.io>
2023-01-10 12:29:44 +0200

    prerequisites

--- datasources/linux/localhost/linux/prerequisites/prerequisites.bash
+++ datasources/linux/localhost/linux/prerequisites/prerequisites.bash
@@ -11,6 +11,7 @@ echo -e "###$PURPLE_COLOR Prerequisites $WHITE_COLOR###"
 echo -e '#####################'
 
 load_prerequisites_utils
+
 MACHINE_NAME='localhost'
 # Check if localhost can connect to Logz.io logs (port 8071)
 execute_task 'can_machine_connect_to_logzio_logs' 'Checking if localhost can connect to Logz.io logs'
