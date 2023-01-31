39fa241d3853fae08867585094b971cd2250812b
AntonKolomiiets <anton.kolomiiets@logz.io>
2023-01-31 12:52:32 +0200

    update page

--- GCP/Logging/Logging/telemetry/logs/linux/functions.bash
+++ GCP/Logging/Logging/telemetry/logs/linux/functions.bash
@@ -194,12 +194,6 @@ function download_logzio_pubsub_integration(){
         return 3
     fi
 
-	mkdir $logzio_temp_dir/integration 2>$task_error_file
-    if [[ $? -ne 0 ]]; then
-        local err=$(cat $task_error_file)
-        write_run "print_error \"logs.bash (1): failed to get last integration file from Github.\n  $err\""
-        return 3
-    fi
 	
     # Unzip Integration release file 
     tar -zxf $logzio_temp_dir/logzio-google-pubsub.zip --directory $logzio_temp_dir 2>$task_error_file
@@ -208,7 +202,7 @@ function download_logzio_pubsub_integration(){
         write_run "print_error \"logs.bash (1): Failed to unzip Integration release file.\n  $err\""
         return 3
     fi
-	
+
     # Add permission to execute file run.sh
     chmod +x $logzio_temp_dir/logzio-google-pubsub/run.sh 2>$task_error_file
     if [[ $? -ne 0 ]]; then
