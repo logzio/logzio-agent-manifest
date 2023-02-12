5ec30f4f87f6a7e5d6bfcd0a2537626328feb69d
ShiranAvidov <shiran.avidov@logz.io>
2023-02-12 12:25:48 +0200

    load another utils from linux resources

--- datasources/linux/localhost/linux/installer/functions.bash
+++ datasources/linux/localhost/linux/installer/functions.bash
@@ -16,6 +16,17 @@ function load_installer_utils {
     send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_PRE_INSTALLATION" "$LOG_SCRIPT_INSTALLER" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE"
     write_log "$LOG_LEVEL_DEBUG" "$message"
 
+    source "$ALL_RESOURCES_LINUX_DIR/subtype_installer_utils.bash" 2>"$TASK_ERROR_FILE"
+    if [[ $? -ne 0 ]]; then
+        message="installer.bash ($EXIT_CODE): error loading installer utils functions: $(get_task_error_message)"
+        send_log_to_logzio "$LOG_LEVEL_ERROR" "$message" "$LOG_STEP_PREREQUISITES" "$LOG_SCRIPT_PREREQUISITES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE"
+        write_error "$message"
+
+        IS_AGENT_FAILED=true
+        run_final
+        exit $EXIT_CODE
+    fi
+    
     source "$OTEL_RESOURCES_LINUX_DIR/subtype_installer_utils.bash" 2>"$TASK_ERROR_FILE"
     if [[ $? -ne 0 ]]; then
         message="installer.bash ($EXIT_CODE): error loading installer utils functions: $(get_task_error_message)"
