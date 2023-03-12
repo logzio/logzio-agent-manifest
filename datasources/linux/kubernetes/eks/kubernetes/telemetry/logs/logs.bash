213e893966f7b7894d04b596764a1edd9826ddeb
ShiranAvidov <shiran.avidov@logz.io>
2023-03-12 13:19:20 +0200

    add trivy

--- datasources/linux/kubernetes/eks/kubernetes/telemetry/logs/logs.bash
+++ datasources/linux/kubernetes/eks/kubernetes/telemetry/logs/logs.bash
@@ -31,3 +31,15 @@ if $IS_FARGATE; then
     # Build enable Fargate Helm set
     execute_task 'build_enable_fargate_helm_set' 'Building enable Fargate Helm set'
 fi
+# Get is scan security risks option was selected
+execute_task "get_is_scan_security_risks_option_was_selected" "Getting is scan security risks option was selected"
+if $IS_SCAN_SECURITY_RISKS_OPTION_WAS_SELCTED; then
+    # Build enable security report Helm set
+    execute_task "build_enable_security_report_helm_set" "Building enable security report Helm set"
+    # Build Logz.io Trivy logs listener URL Helm set
+    execute_task "build_logzio_trivy_logs_listener_url_helm_set" "Building Logz.io Trivy logs listener URL Helm set"
+    # Build Logz.io Trivy logs token Helm set
+    execute_task "build_logzio_trivy_logs_token_helm_set" "Building Logz.io Trivy logs token Helm set"
+    # Build Trivy environment id Helm set
+    execute_task "build_trivy_environment_id_helm_set" "Building Trivy environment id Helm set"
+fi
--- resources-linux/k8s/datasource_logs_utils.bash
+++ resources-linux/k8s/datasource_logs_utils.bash
@@ -95,3 +95,130 @@ function build_environment_id_helm_set {
     write_task_post_run "LOG_HELM_SETS+='$helm_set'"
     write_task_post_run "HELM_SETS+='$helm_set'"
 }
+
+# Gets is scan security risks option was selected
+# Input:
+#   ---
+# Output:
+#   IS_SCAN_SECURITY_RISKS_OPTION_WAS_SELCTED - Tells if scan security risks option was selected (true/false)
+function get_is_scan_security_risks_option_was_selected {
+    local func_name="${FUNCNAME[0]}"
+
+    local message='Checking if scan security risks option was selected ...'
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    PARAMS=("${LOGS_PARAMS[@]}")
+    get_param_value 'isScanSecurityRisks'
+    if [[ $? -ne 0 ]]; then
+        message="logs.bash ($EXIT_CODE): $(get_task_error_message)"
+        send_log_to_logzio "$LOG_LEVEL_ERROR" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_task_post_run "write_error \"$message\""
+
+        return $EXIT_CODE
+    fi
+
+    local is_scan_security_risks=$PARAM_VALUE
+
+    if is_scan_security_risks; then
+        message='Scan security risks option was selected'
+    else
+        message='Scan security risks option was not selected'
+    fi
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    write_task_post_run "IS_SCAN_SECURITY_RISKS_OPTION_WAS_SELCTED=$is_scan_security_risks"
+}
+
+# Builds enable security report Helm set
+# Input:
+#   ---
+# Output:
+#   LOG_HELM_SETS - Contains all the Helm sets for logging
+#   HELM_SETS - Contains all the Helm sets
+function build_enable_security_report_helm_set {
+    local func_name="${FUNCNAME[0]}"
+
+    local message='Building enable security report Helm set ...'
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    local helm_set=" --set securityReport.enabled=true"
+
+    message="Enable security report Helm set is '$helm_set'"
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    write_task_post_run "LOG_HELM_SETS+='$helm_set'"
+    write_task_post_run "HELM_SETS+='$helm_set'"
+}
+
+# Builds Logz.io Trivy logs listener URL Helm set
+# Input:
+#   ---
+# Output:
+#   LOG_HELM_SETS - Contains all the Helm sets for logging
+#   HELM_SETS - Contains all the Helm sets
+function build_logzio_trivy_logs_listener_url_helm_set {
+    local func_name="${FUNCNAME[0]}"
+
+    local message='Building Logz.io Trivy logs listener url Helm set ...'
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    local helm_set=" --set logzio-trivy.secrets.logzioListener=$LISTENER_URL"
+
+    message="Logz.io Trivy logs listener url Helm set is '$helm_set'"
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    write_task_post_run "LOG_HELM_SETS+='$helm_set'"
+    write_task_post_run "HELM_SETS+='$helm_set'"
+}
+
+# Builds Logz.io Trivy logs token Helm set
+# Input:
+#   ---
+# Output:
+#   LOG_HELM_SETS - Contains all the Helm sets for logging
+#   HELM_SETS - Contains all the Helm sets
+function build_logzio_trivy_logs_token_helm_set {
+    local func_name="${FUNCNAME[0]}"
+
+    local message='Building Logz.io Trivy logs token Helm set ...'
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    local helm_set=" --set logzio-trivy.secrets.logzioShippingToken=$LOGS_TOKEN"
+
+    message="Logz.io Trivy logs token Helm set is '$helm_set'"
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    write_task_post_run "LOG_HELM_SETS+='$helm_set'"
+    write_task_post_run "HELM_SETS+='$helm_set'"
+}
+
+# Builds Trivy environment id helm set
+# Input:
+#   ---
+# Output:
+#   LOG_HELM_SETS - Contains all the Helm sets for logging
+#   HELM_SETS - Contains all the Helm sets
+function build_trivy_environment_id_helm_set {
+    local func_name="${FUNCNAME[0]}"
+
+    local message='Building Trivy environment id Helm set ...'
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    local helm_set=" --set logzio-trivy.env_id=$ENV_ID"
+
+    message="Trivy environment id Helm set is '$helm_set'"
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_LOGS" "$LOG_SCRIPT_LOGS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    write_task_post_run "LOG_HELM_SETS+='$helm_set'"
+    write_task_post_run "HELM_SETS+='$helm_set'"
+}
