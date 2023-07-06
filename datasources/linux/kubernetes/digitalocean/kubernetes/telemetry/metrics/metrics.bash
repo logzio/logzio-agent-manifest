03d345ade320e73abafddb99297eca541c572584
ShiranAvidov <48657224+ShiranAvidov@users.noreply.github.com>
2023-07-06 11:38:34 +0300

    fix k8s filter (#102)

--- datasources/linux/kubernetes/digitalocean/kubernetes/telemetry/metrics/metrics.bash
+++ datasources/linux/kubernetes/digitalocean/kubernetes/telemetry/metrics/metrics.bash
@@ -27,5 +27,6 @@ execute_task 'build_logzio_metrics_token_helm_set' 'Building Logz.io metrics tok
 execute_task 'get_is_metrics_filter_was_selected' 'Getting is metrics filter was selected'
 if $IS_FILTER; then
     # Build enable metrics filter Helm set
+    CLOUD_PROVIDER='aks'
     execute_task 'build_enable_metrics_filter_helm_set' 'Building enable metrics filter Helm set'
 fi
