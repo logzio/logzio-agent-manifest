cdf5687443511dcdbdb0864c27ced3325c6412b4
ralongit <ralgotmail@gmail.com>
2023-10-03 11:00:35 +0300

    Support EC2 for windows - first release
    - Create relevant powershell scripts
    - Update agent manifest
    - Create a dir for windows EC2

---
+++ datasources/windows/aws/ec2/prerequisites/prerequisites.ps1
@@ -0,0 +1,21 @@
+#################################################################################################################################
+############################################# WINDOWS Subtype Prerequisites Script ##############################################
+#################################################################################################################################
+
+$local:PrerequisitesScriptFile = "$LogzioTempDir\$Platform\$SubType\$PrerequisitesFunctionsFile"
+
+# Print title
+Write-Host
+Write-Host '#####################'
+Write-Host '### ' -NoNewline
+Write-Host 'Prerequisites' -ForegroundColor Magenta -NoNewline
+Write-Host ' ###'
+Write-Host '#####################'
+
+# Check if localhost can connect to Logz.io logs (port 8071)
+Invoke-Task 'Test-CanLocalhostConnectToLogzioLogs' @{ListenerUrl = $script:ListenerUrl} 'Checking if localhost can connect to Logz.io logs' @($PrerequisitesScriptFile)
+# Check if localhost can connect to Logz.io metrics (port 8053)
+Invoke-Task 'Test-CanLocalhostConnectToLogzioMetrics' @{ListenerUrl = $script:ListenerUrl} 'Checking if localhost can connect to Logz.io metrics' @($PrerequisitesScriptFile)
+
+# Finished successfully
+Exit 0
