e1a731c4119d37d6aa3cbcf54ce38ec3100ad34d
8naama <naama.bendalak@logz.io>
2025-01-21 18:28:21 +0200

    update windows helm command

--- datasources/windows/kubernetes/eks/deployevents/telemetry/logs/logs.ps1
+++ datasources/windows/kubernetes/eks/deployevents/telemetry/logs/logs.ps1
@@ -12,16 +12,12 @@ Write-Host 'Logs' -ForegroundColor Magenta -NoNewline
 Write-Host ' ###'
 Write-Host '############'
 
-# Get Logz.io logs token
-Invoke-Task 'Get-LogzioLogsToken' @{} 'Getting Logz.io logs token' @($LogsFunctionsScript)
 # Build enable deploy events Helm set
 Invoke-Task 'Build-EnableDeployEventsHelmSet' @{} 'Building enable deploy events Helm set' @($LogsFunctionsScript)
-# Build Logz.io Deploy events logs listener url Helm set
-Invoke-Task 'Build-LogzioDeployEventsLogsListenerUrlHelmSet' @{ListenerUrl = $script:ListenerUrl} 'Building Logz.io deploy events logs listener url Helm set' @($LogsFunctionsScript)
 # Build Logz.io Deploy events logs token Helm set
-Invoke-Task 'Build-LogzioDeployEventsLogsTokenHelmSet' @{LogsToken = $script:LogsToken} 'Building Logz.io deploy events logs token Helm set' @($LogsFunctionsScript)
-# Build Deploy events environment id Helm set
-Invoke-Task 'Build-DeployEventsEnvironmentIdHelmSet' @{EnvId = $script:EnvId} 'Building Deploy events environment id Helm set' @($LogsFunctionsScript)
+. ..\..\..\kubernetes\telemetry\logs\functions.ps1
+Invoke-Task 'Get-LogzioLogsToken' @{} 'Getting Logz.io logs token' @($LogsFunctionsScript)
+Invoke-Task 'Build-LogzioLogsTokenHelmSet' @{LogsToken = $script:LogsToken} 'Building Logz.io deploy events logs token Helm set' @($LogsFunctionsScript)
 
 # Finished successfully
 Exit 0
--- resources-linux/k8s/datasource_metrics_utils.bash
+++ resources-linux/k8s/datasource_metrics_utils.bash
@@ -27,30 +27,6 @@ function build_enable_metrics_helm_set {
     write_task_post_run "HELM_SETS+='$helm_set'"
 }
 
-# Builds Logz.io metrics listener URL Helm set
-# Input:
-#   ---
-# Output:
-#   LOG_HELM_SETS - Contains all the Helm sets for logging
-#   HELM_SETS - Contains all the Helm sets
-function build_logzio_metrics_listener_url_helm_set {
-    local func_name="${FUNCNAME[0]}"
-
-    local message='Building Logz.io metrics listener url Helm set ...'
-    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_METRICS" "$LOG_SCRIPT_METRICS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
-    write_log "$LOG_LEVEL_DEBUG" "$message"
-
-    local listener_url="https://$LISTENER_URL:8053"
-    local helm_set=" --set logzio-k8s-telemetry.secrets.ListenerHost=$listener_url"
-
-    local message="Logz.io metrics listener url Helm set is '$helm_set'"
-    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_METRICS" "$LOG_SCRIPT_METRICS" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
-    write_log "$LOG_LEVEL_DEBUG" "$message"
-
-    write_task_post_run "LOG_HELM_SETS+='$helm_set'"
-    write_task_post_run "HELM_SETS+='$helm_set'"
-}
-
 # Builds Logz.io metrics token Helm set
 # Input:
 #   ---
