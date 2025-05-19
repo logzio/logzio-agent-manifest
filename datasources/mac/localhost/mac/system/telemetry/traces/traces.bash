a418c1fc25020f381929b00481176792ece121f1
Yotam loewenbach <yotam.loewenbach@logz.io>
2025-05-19 12:15:16 +0700

    Handle traces params mac + linux + ec2

--- datasources/mac/localhost/mac/system/telemetry/traces/traces.bash
+++ datasources/mac/localhost/mac/system/telemetry/traces/traces.bash
@@ -15,6 +15,14 @@ EXIT_CODE=1
 # Load datasource traces utils functions
 load_traces_utils
 
+# Load datasource traces utils functions
+get_is_span_metrics
+get_sampling_latency
+get_sampling_propability
+export IS_SPAN_METRICS
+export SAMPLING_LATENCY
+export SAMPLING_PROPABILITY
+
 # Get Logz.io traces token
 execute_task 'get_logzio_traces_token' 'Getting Logz.io traces token'
 # Add traces pipeline to OTEL config
@@ -22,7 +30,6 @@ execute_task 'add_traces_pipeline_to_otel_config' 'Adding traces pipeline to OTE
 # Get traces OTEL receivers (OTLP)
 execute_task 'get_traces_otel_receivers' 'Getting traces OTEL receivers'
 # Add traces receivers to OTEL config
-TRACES_TYPE='agent-localhost-mac'
 execute_task 'add_traces_receivers_to_otel_config' 'Adding traces receivers to OTEL config'
 # Get traces OTEL processors
 execute_task 'get_traces_otel_processors' 'Getting traces OTEL processors'
@@ -30,20 +37,10 @@ execute_task 'get_traces_otel_processors' 'Getting traces OTEL processors'
 execute_task 'add_traces_processors_to_otel_config' 'Adding traces processors to OTEL config'
 # Add traces exporter to OTEL config (Logz.io)
 execute_task 'add_traces_exporter_to_otel_config' 'Adding traces exporter to OTEL config'
-
-# Print span metrics title
-echo
-echo -e '##############'
-echo -e "###$PURPLE_COLOR Span Metrics $WHITE_COLOR###"
-echo -e '##############'
-
-# Get Logz.io metrics token
-execute_task 'get_logzio_metrics_token' 'Getting Logz.io metrics token'
-# Add spanmetrics pipeline to OTEL config
-execute_task 'add_spanmetrics_pipeline_to_otel_config' 'Adding span metrics pipeline to OTEL config'
-# Add spanmetrics connector to OTEL config
-execute_task 'add_spanmetrics_connector_to_otel_config' 'Adding span metrics connector to OTEL config'
-# Add spanmetrics processors to OTEL config
-execute_task 'add_spanmetrics_processors_to_otel_config' 'Adding span metrics processors to OTEL config'
-# Add spanmetrics exporter to OTEL config
-execute_task 'add_spanmetrics_exporter_to_otel_config' 'Adding span metrics exporter to OTEL config'
+# Configure span metrics 
+if [[ "$IS_SPAN_METRICS" == "true" ]]; then
+  execute_task 'add_spanmetrics_pipeline_to_otel_config' 'Adding span metrics pipeline to OTEL config'
+  execute_task 'add_spanmetrics_connector_to_otel_config' 'Adding span metrics connector to OTEL config'
+  execute_task 'add_spanmetrics_processors_to_otel_config' 'Adding span metrics processors to OTEL config'
+  execute_task 'add_spanmetrics_exporter_to_otel_configs' 'Adding span metrics exporter to OTEL config'
+fi
--- resources-mac/all/datasource_traces_utils.bash
+++ resources-mac/all/datasource_traces_utils.bash
@@ -63,3 +63,77 @@ function get_logzio_metrics_token {
 
     write_task_post_run "METRICS_TOKEN='$shipping_token'"
 }
+# Gets the value for isSpanMetrics param
+# Output: sets IS_SPAN_METRICS as "true" or "false" (string)
+function get_is_span_metrics {
+    local func_name="${FUNCNAME[0]}"
+    local message="Getting 'isSpanMetrics' param for span metrics collection ..."
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    PARAMS=("${TRACES_PARAMS[@]}")
+    get_param_value 'isSpanMetrics'
+    local rc=$?
+    if [[ $rc -ne 0 ]]; then
+        IS_SPAN_METRICS="false"
+        message="'isSpanMetrics' param not found, defaulting to false"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    else
+        IS_SPAN_METRICS="${PARAM_VALUE}"
+        message="'isSpanMetrics' param found: $IS_SPAN_METRICS"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    fi
+    export IS_SPAN_METRICS
+}
+
+# Gets the value for samplingLatency param
+# Output: sets SAMPLING_LATENCY as number (or default 0)
+function get_sampling_latency {
+    local func_name="${FUNCNAME[0]}"
+    local message="Getting 'samplingLatency' param for trace sampling latency threshold ..."
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    PARAMS=("${TRACES_PARAMS[@]}")
+    get_param_value 'samplingLatency'
+    local rc=$?
+    if [[ $rc -ne 0 ]]; then
+        SAMPLING_LATENCY=200
+        message="'samplingLatency' param not found, defaulting to 200"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    else
+        SAMPLING_LATENCY="${PARAM_VALUE}"
+        message="'samplingLatency' param found: $SAMPLING_LATENCY"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    fi
+    export SAMPLING_LATENCY
+}
+
+# Gets the value for samplingPropability param
+# Output: sets SAMPLING_PROPABILITY as number (or default 0)
+function get_sampling_propability {
+    local func_name="${FUNCNAME[0]}"
+    local message="Getting 'samplingPropability' param for trace sampling probability ..."
+    send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+    write_log "$LOG_LEVEL_DEBUG" "$message"
+
+    PARAMS=("${TRACES_PARAMS[@]}")
+    get_param_value 'samplingPropability'
+    local rc=$?
+    if [[ $rc -ne 0 ]]; then
+        SAMPLING_PROPABILITY=10
+        message="'samplingPropability' param not found, defaulting to 10"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    else
+        SAMPLING_PROPABILITY="${PARAM_VALUE}"
+        message="'samplingPropability' param found: $SAMPLING_PROPABILITY"
+        send_log_to_logzio "$LOG_LEVEL_DEBUG" "$message" "$LOG_STEP_TRACES" "$LOG_SCRIPT_TRACES" "$func_name" "$AGENT_ID" "$PLATFORM" "$SUB_TYPE" "$CURRENT_DATA_SOURCE"
+        write_log "$LOG_LEVEL_DEBUG" "$message"
+    fi
+    export SAMPLING_PROPABILITY
+}
