78adfc59ce4ee55dc9501cb9808a9af6a963ddc8
Yotam loewenbach <yotam.loewenbach@logz.io>
2025-06-08 19:57:53 +0700

    Get general params

--- resources-linux/otel/subtype_installer_utils.bash
+++ resources-linux/otel/subtype_installer_utils.bash
@@ -215,7 +215,6 @@ function copy_logzio_otel_collector_service_file_to_systemd_system_dir {
     # Extract envID from GENERAL_PARAMS
     local envid_value=""
     for param in ${GENERAL_PARAMS//,/ }; do
-        echo "observing param: $param"
         if [[ "$param" == envID=* ]]; then
             envid_value="${param#envID=}"
             break
